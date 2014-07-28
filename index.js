7ac57aff16e6ddbd56be01a015de0f73931913b7
Calvin Metcalf <cmetcalf@appgeo.com>
2014-07-28 08:24:20 -0400

    length

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 'use strict';
-var fs = require('fs');
-var wordListPath = require('word-list');
-var words = fs.readFileSync(wordListPath, 'utf8').split('\n');
-var uniqueRandom = require('unique-random')(0, words.length - 1);
-
-module.exports = function () {
-	return words[uniqueRandom()];
+var words = require('word-list-json');
+var uniqueRandom = require('unique-random');
+var uniqueRandoms = {
+  full: uniqueRandom(0, words.length - 1)
 };
+var maxLen = 0;
+Object.keys(words.lengths).forEach(function (len) {
+  if (Number(len) > maxLen) {
+    maxLen = len;
+  }
+  uniqueRandoms[len] = uniqueRandom(0, words.lengths[len] - 1);
+});
+function randomWord(len) {
+  if (typeof len !== 'number' || len < 2 || len !== len || len > maxLen) {
+    len = 'full';
+  }
+  while (!(len in uniqueRandoms)) {
+    len--;
+  }
+  
+  return words[uniqueRandoms[len]()];
+}
+randomWord.maxLen = maxLen;
+randomWord.uniqueRandoms = uniqueRandoms;
+module.exports = randomWord;
--- test.js
+++ test.js
@@ -5,3 +5,33 @@ var randomWord = require('./index');
 it('should return a random word', function () {
 	assert(randomWord().length > 1);
 });
+
+describe ('should respect length', function () {
+  it (2, function () {
+    var i = 200;
+    while (--i) {
+      assert.equal(randomWord(2).length, 2);
+    }
+  });
+  var j = 2;
+  var len = 30;
+  while (++j < len) {
+    (function (){
+      var k = j;
+      it (k, function () {
+        var i = 200;
+        while (--i) {
+          assert(randomWord(k).length <= k);
+        }
+      });
+    }());
+  }
+
+});
+
+it ('should ignore a non-number', function () {
+    var i = 200;
+    while (--i) {
+      assert(randomWord('foo').length > 1);
+    }
+  });
